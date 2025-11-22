691195b5fc6f919707caa1bec9d1d082d3b50146
Umut Yunusoğlu <umut.yunusoglu@sabanciuniv.edu>
2025-11-22 17:04:23 +0300

    Upload Post Usecase

--- lib/domain/services/file_service.dart
+++ lib/domain/services/file_service.dart
@@ -2,14 +2,21 @@ abstract class FileService {
   static const String rootPublic = 'Public/';
   static const String rootPrivate = 'Private/';
 
-  static const String publicAppAssets = '${rootPublic}App/Assets/';
-  static const String publicImages = '${publicAppAssets}Images/';
-  static const String publicVideos = '${publicAppAssets}Videos/';
+  static const String publicAppAssets = '$rootPublic/assets/';
+  static const String publicImages = '${publicAppAssets}images/';
+  static const String publicVideos = '${publicAppAssets}videos/';
 
   static const String privateEvents = '${rootPrivate}Events/';
   static const String privateUsers = '${rootPrivate}Users/';
+
   static String userProfileImagePath(String userId, String fileName) =>
-      '$privateUsers$userId/Profile/Images/$fileName';
+      '$privateUsers$userId/profile/images/$fileName';
+
+  static String postImagePath(
+    String userId,
+    String postId,
+    String fileName,
+  ) => '$privateUsers$userId/posts/$postId/images/$fileName';
 
   Future<String> uploadFile(
     String absoluteSourcePath,
