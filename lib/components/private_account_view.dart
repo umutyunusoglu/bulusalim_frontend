502364444d0cc9155b42083ef3776a386ff858cc
Hatice Guler <hg3562062@gmail.com>
2026-01-27 21:24:06 +0300

    profile changes

---
+++ lib/components/private_account_view.dart
@@ -0,0 +1,55 @@
+import 'package:bulusalim/core/constants/theme/color_themes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class PrivateAccountView extends StatelessWidget {
+  const PrivateAccountView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 100.w,
+            height: 100.w,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: AppColors.primaryColor,
+                width: 3.w,
+              ),
+            ),
+            child: Icon(
+              Icons.lock_outline,
+              size: 50.sp,
+              color: AppColors.primaryColor,
+            ),
+          ),
+          SizedBox(height: 16.h),
+          Text(
+            'Gizli Hesap',
+            style: TextStyle(
+              fontFamily: 'SF Pro Display',
+              fontSize: 15.sp,
+              fontWeight: FontWeight.w500,
+              color: AppColors.primaryColor,
+            ),
+          ),
+          SizedBox(height: 8.h),
+          Text(
+            'Bu hesap ile etkileşime geçebilmek için\ntakip etmelisin.',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontFamily: 'SF Pro Display',
+              fontSize: 12.sp,
+              fontWeight: FontWeight.w400,
+              color: const Color(0xFF8E8E93),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/profile/grid_tab.dart
+++ lib/screens/profile/grid_tab.dart
@@ -1,5 +1,5 @@
 import 'package:bulusalim/core/utils/debug/android_image_url_fixer.dart';
-import 'package:bulusalim/domain/entities/user/pinned_post_entity.dart'; // UserPostEntity
+import 'package:bulusalim/domain/entities/user/pinned_post_entity.dart';
 import 'package:bulusalim/screens/profile/profile_feed_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -28,8 +28,8 @@ class ProfileGridTab extends StatelessWidget {
     // 3. Listeleri birleştir (Sıralama: Önce Active, Sonra Pinned)
     final allPosts = [...markedActivePosts, ...markedPinnedPosts];
 
-    Navigator.push(
-      context,
+    // Sayfayı Navbar'ın üzerinde aç (rootNavigator: true)
+    Navigator.of(context, rootNavigator: true).push(
       MaterialPageRoute(
         builder: (context) => ProfilePostFeedPage(
           posts: allPosts,
@@ -41,68 +41,72 @@ class ProfileGridTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Grid gösterimi için ham listeleri birleştir
     final totalPosts = [...activePosts, ...pinnedPosts];
+    final theme = Theme.of(context);
 
-    return GridView.builder(
-      padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        crossAxisSpacing: 16.w,
-        mainAxisSpacing: 16.h,
-      ),
-      itemCount: totalPosts.length,
-      itemBuilder: (context, index) {
-        final bool isPinnedItem = index >= activePosts.length;
+    return ColoredBox(
+      color: theme.colorScheme.surface,
+      child: GridView.builder(
+        padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 16.w,
+          mainAxisSpacing: 16.h,
+        ),
+        itemCount: totalPosts.length,
+        itemBuilder: (context, index) {
+          final isPinnedItem = index >= activePosts.length;
 
-        final iconData = isPinnedItem
-            ? Icons.push_pin
-            : Icons.access_time_filled;
+          final iconData = isPinnedItem
+              ? Icons.push_pin
+              : Icons.access_time_filled;
 
-        final post = totalPosts[index];
+          final post = totalPosts[index];
 
-        return GestureDetector(
-          onTap: () => _openFeedPage(context, index),
-          child: Stack(
-            fit: StackFit.expand,
-            children: [
-              // 1. FOTOĞRAF
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16.r),
-                  image: DecorationImage(
-                    image: NetworkImage(
-                      fixEmulatorUrl(post.imageUrls.first),
-                    ),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-
-              // 2. SAĞ ÜST İKON (Dinamik)
-              Positioned(
-                top: 6.h,
-                right: 4.w,
-                child: Container(
-                  padding: EdgeInsets.all(6.r),
-                  child: Icon(
-                    iconData,
-                    color: Colors.white,
-                    size: 16.sp,
-                    shadows: const [
-                      Shadow(
-                        color: Colors.black45,
-                        blurRadius: 4,
-                        offset: Offset(1, 1),
+          return GestureDetector(
+            onTap: () => _openFeedPage(context, index),
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                // 1. FOTOĞRAF
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16.r),
+                    color: theme.colorScheme.surfaceContainerHighest,
+                    image: DecorationImage(
+                      image: NetworkImage(
+                        fixEmulatorUrl(post.imageUrls.first),
                       ),
-                    ],
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-        );
-      },
+
+                // 2. SAĞ ÜST İKON (Dinamik)
+                Positioned(
+                  top: 6.h,
+                  right: 4.w,
+                  child: Container(
+                    padding: EdgeInsets.all(6.r),
+                    child: Icon(
+                      iconData,
+                      color: Colors.white,
+                      size: 16.sp,
+                      shadows: const [
+                        Shadow(
+                          color: Colors.black45,
+                          blurRadius: 4,
+                          offset: Offset(1, 1),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
