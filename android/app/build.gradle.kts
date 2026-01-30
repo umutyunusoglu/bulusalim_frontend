585dee68daeb1dd7938f1eb9bb10591d7254c291
Umut Yunusoğlu <umut.yunusoglu@sabanciuniv.edu>
2026-01-30 14:14:31 +0300

    version update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -61,8 +61,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     buildTypes {
