3a8efc8bbc9e3ed992a599901694fff589b237e8
umutyunusoglu <yunusoglumut@gmail.com>
2026-02-10 11:16:23 +0300

    android version

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:ui';
-
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
