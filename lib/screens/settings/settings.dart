f8b5d0ea76835dac5e0de5aa626fbbb7334ec475
umutyunusoglu <yunusoglumut@gmail.com>
2026-02-15 02:07:17 +0300

    notifications

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -90,7 +90,7 @@ class _SettingsPageState extends State<SettingsPage>
       ),
     );
     // Sayfadan geri dönüldüğünde stateleri güncelle
-    _checkAllPermissions();
+    await _checkAllPermissions();
   }
 
   // --- POPUP FONKSİYONU ---
@@ -245,6 +245,7 @@ class _SettingsPageState extends State<SettingsPage>
                 permission: Permission.notification,
               ),
             ),
+            /*
             SettingsTile(
               title: 'Bluetooth',
               trailingText: getStatusText(_isBluetoothGranted), // DİNAMİK
@@ -254,7 +255,7 @@ class _SettingsPageState extends State<SettingsPage>
                     "Outnest uygulamasına yakındaki cihazlarla bağlantı kurmak için Bluetooth'u kullanmasına izin verilir.",
                 permission: Permission.bluetooth,
               ),
-            ),
+            ),*/
             SettingsTile(
               title: 'Konum Servisleri',
               trailingText: getStatusText(_isLocationGranted), // DİNAMİK
