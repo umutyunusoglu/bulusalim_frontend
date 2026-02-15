856bd42eb11058927113a5b3857b9f4bb657b70c
umutyunusoglu <yunusoglumut@gmail.com>
2026-02-15 23:36:52 +0300

    android version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -62,8 +62,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.0.8+hotfix4"
+        versionCode = 16
+        versionName = "1.0.9"
         multiDexEnabled = true
     }
 
