ffc1acf34ec2a5c82f129b512aa832e5933ca1cb
Umut Yunusoğlu <yunusoglumut@gmail.com>
2026-03-14 13:08:24 +0300

    16kb android page size

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,8 +35,18 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "app.outnest"
     compileSdk = 36
-    ndkVersion = flutter.ndkVersion
-
+    ndkVersion = "29.0.14206865"
+    packaging {
+        jniLibs {
+            // Bu satır yerel kütüphanelerin sıkıştırılmadan, 
+            // sayfa sınırlarına uygun paketlenmesini sağlar.
+            useLegacyPackaging = false
+            
+        }
+        resources {
+                excludes += "**/lib/**/gdbserver"
+            }
+        }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -62,9 +72,12 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26
         targetSdk = 35
-        versionCode = 22
-        versionName = "1.0.14+hotfix"
+        versionCode = 23
+        versionName = "1.1.0"
         multiDexEnabled = true
+
+       
+    
     }
 
     buildTypes {
