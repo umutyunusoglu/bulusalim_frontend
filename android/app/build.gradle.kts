1e6063713ff8b2b3a06922482050068977a0ba12
Umut Yunusoğlu <yunusoglumut@gmail.com>
2026-03-22 23:01:41 +0300

    android update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -72,8 +72,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26
         targetSdk = 35
-        versionCode = 24
-        versionName = "1.1.0"
+        versionCode = 26
+        versionName = "1.1.1"
         multiDexEnabled = true
 
         externalNativeBuild {
