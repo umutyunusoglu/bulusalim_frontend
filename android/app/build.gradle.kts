093f7ae112a9d9a36262815599ff07e8f64b9ce1
Umut Yunusoğlu <yunusoglumut@gmail.com>
2026-03-29 15:16:29 +0300

    gitignore

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -72,8 +72,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26
         targetSdk = 35
-        versionCode = 28
-        versionName = "1.1.3"
+        versionCode = 29
+        versionName = "1.1.4"
         multiDexEnabled = true
 
         externalNativeBuild {
