72f75394ff8e489d9a2d5f4a09f2cbe3d77bbc9a
ilke demirkir <>
2026-04-02 19:54:36 +0300

    Share Link Bug Fix, Notification update will be in the next PR

--- lib/domain/services/share_links_service.dart
+++ lib/domain/services/share_links_service.dart
@@ -1,12 +1,16 @@
 // This is the service responsible for generating share links for posts and events and parsing incoming share links to navigate to the appropriate content within the app.
-import 'package:flutter/widgets.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:outnest/data/models/links/deep_link_target.dart';
-
 abstract class ShareLinksService {
   Future<void> sharePost(String postId);
 
   Future<void> shareEvent(String eventId);
 
   Future<void> shareUserProfile(String userId);
+
+  void setPendingDeepLink(String? deepLink);
+
+  String? getPendingDeepLink();
+
+  void clearPendingDeepLink();
+
+  bool hasPendingDeepLink();
 }
