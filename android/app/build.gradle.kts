a8e76275b147c45a0b6aaac280fd9ad3f20f7044
ilke demirkir <>
2026-04-11 20:30:46 +0300

    Test #8 Version code needed to be updated from build.gradle.kts and pubspec.yaml.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -72,7 +72,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26
         targetSdk = 35
-        versionCode = 31
+        versionCode = 32
         versionName = "1.1.5"
         multiDexEnabled = true                                                                                                                                                                                   
 
