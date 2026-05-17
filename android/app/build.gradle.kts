0810ede4e31ab4f0f314c69a2f24645709c07932
ilke demirkir <>
2026-05-17 21:51:59 +0300

    Versioning fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,7 +39,7 @@ if (localPropertiesFile.exists()) {
 }
 
 val flutterVersionCode = localProperties.getProperty("flutter.versionCode")?.toInt() ?: 1
-val flutterVersionName = "1.1.9"
+val flutterVersionName = "1.1.10"
 
 android {
     namespace = "app.outnest"
